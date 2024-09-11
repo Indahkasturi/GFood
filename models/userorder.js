c5b37ae886f0ba91c7cf2cd3b11bb4d174836ec4
indahkasturi <indahkasturiii@gmail.com>
2024-09-12 00:34:01 +0700

    relasi

---
+++ models/userorder.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserOrder extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  UserOrder.init({
+    UserId: DataTypes.INTEGER,
+    ProductId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserOrder',
+  });
+  return UserOrder;
+};
---
+++ seeders/20240911153012-seeduser.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { DATE } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const users = require('../data/user.json').map(el => {
+      el.createdAt = el.updatedAt = new Date()
+      return el
+    })
+    await queryInterface.bulkInsert("Users", users)
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null)
+  }
+};
