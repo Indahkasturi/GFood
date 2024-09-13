ceb505bbca3d596017ba18276c074870ab29f800
indahkasturi <indahkasturiii@gmail.com>
2024-09-13 08:58:46 +0700

    commit

--- controller/controller.js
+++ controller/controller.js
@@ -2,6 +2,7 @@ const { User, Profile, Product, UserOrder } = require("../models/index")
 const { Op } = require('sequelize')
 const { compareHashed } = require('../helper/helper')
 const sendBulkEmails = require('../mvp/nodeMailer')
+const product = require("../models/product")
 
 class Controller {
     static async home(req, res) {
@@ -28,19 +29,19 @@ class Controller {
             let { email, password } = req.body;
             await User.findOne({ where: { email } })
                 .then(async data => {
-                    
+
                     if (data) {
                         const validatePassword = await compareHashed(password, data.password);
-                        
-                        
+
+
                         if (validatePassword) {
-                            
+
                             // console.log(req.session);
                             req.session.userId = data.id
                             console.log(data.id);
-                            
+
                             return res.redirect('/products')
-                        
+
                         } else {
                             let errorMsg = "Incorrect email or password";
                             return res.redirect(`/login?error=${errorMsg}`);
@@ -133,11 +134,11 @@ class Controller {
         try {
             let { id } = req.params
             let data = await Profile.findOne({
-                where:{UserId: +id}
+                where: { UserId: +id }
             }) //{"id":1,"name":"saya","address":"Jakarta Barat","age":20,"UserId":1}
             // res.send(data)
             // id = +id
-            
+
             res.render('profileEdit', { data, id })
         } catch (error) {
             res.send(error.message)
@@ -145,10 +146,10 @@ class Controller {
     }
     static async postEditProfile(req, res) {
         try {
-            
+
             console.log(req.params);
             let { id } = req.params
-            
+
             let { name, address, age } = req.body
             await Profile.update({ name, address, age }, { where: { id: +id } })
             let data = await Profile.findByPk(+id)
@@ -163,11 +164,20 @@ class Controller {
     // *START Cart
     static async cart(req, res) {
         try {
-            const buy = await UserOrder.findAll({
-                include: Product
+            const data = await UserOrder.findAll({
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id']
+                    },
+                    {
+                        model: Product,
+                        attributes: ['id', 'name', 'price']
+                    }
+                ]
             })
-            res.send('Render ke "cart" ')
-            // res.render('cart', {})
+            // res.send('Render ke "cart" ')?
+            res.render('cart', { data })
         } catch (error) {
             res.send(error.message)
         }
@@ -201,7 +211,7 @@ class Controller {
             let { id } = req.params
             let deleteProfile = await Product.findByPk(+id)
 
-            await Product.destroy({ where: { id : +id} })
+            await Product.destroy({ where: { id: +id } })
             res.redirect('/products')
             // res.send('Render ke "cart" ')
             // res.render('cart', {})
